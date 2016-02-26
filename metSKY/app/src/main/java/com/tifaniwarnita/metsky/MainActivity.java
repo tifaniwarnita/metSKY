144e5db441e316dddf9716560b05fa68c60a48b2
Tifani Warnita <tiffayumuyuka@gmail.com>
2016-02-26 12:03:14 +0700

    Edit splash screen

--- metSKY/app/src/main/java/com/tifaniwarnita/metsky/MainActivity.java
+++ metSKY/app/src/main/java/com/tifaniwarnita/metsky/MainActivity.java
@@ -23,9 +23,6 @@ public class MainActivity extends AppCompatActivity {
         FirebaseConfig.initialize();
 
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
     }
 
     @Override
